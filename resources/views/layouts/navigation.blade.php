459d2070e3b43e9f4d4191c04919470b50274256
Nanako-I <milklmi.1103775@gmail.com>
2023-12-17 15:43:37 +0000

    html追加

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -20,8 +20,8 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ url('people') }}" >
-                        <!--<x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />-->
-                        <img src="{{ asset('storage/sample/favicon.ico.png') }}" width ="60" height="60">
+                       
+                        <img src="{{ asset('storage/sample/pink-heart.png') }}" width ="60" height="60">
                     </a>
                 </div>
 
