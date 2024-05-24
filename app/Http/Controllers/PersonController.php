ab5c24a318451951f532b6b835a80521c086c2b3
KawashimaYasuhiro <yasu.23721740311@gmail.com>
2024-05-24 19:47:25 +0900

    挙動止まってしまうのでdd削除

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use App\Models\User;
@@ -16,6 +17,7 @@ use Illuminate\Http\Request;
 use App\Enums\PermissionType;
 use App\Enums\RoleType as RoleEnums;
 use App\Enums\Role as RoleEnum;
+
 class PersonController extends Controller
 {
     /**
@@ -25,22 +27,21 @@ class PersonController extends Controller
      */
     public function index()
     {
-        
-   // ログインしているユーザーの情報↓
+
+        // ログインしているユーザーの情報↓
         $user = auth()->user();
-    
+
         $user->facility_staffs()->first();
-    
+
         // facility_staffsメソッドからuserの情報をゲットする↓
         $facilities = $user->facility_staffs()->get();
-        
+
         // dd($facilities);
         $roles = $user->user_roles()->get(); // これでロールが取得できる
         //   dd($roles);
-        
+
         $rolename = $user->getRoleNames(); // ロールの名前を取得
-        dd($rolename);
-        
+
         $isSuperAdmin = $user->hasRole(RoleType::SuperAdministrator);
 
         // ロールのIDを取得する場合
@@ -54,82 +55,82 @@ class PersonController extends Controller
         // $roles = $user->getRoleNames();
         // $permissions = $user->getAllPermissions()->pluck('name');
         // dd(compact('roles', 'permissions'));
-        
-    
+
+
         $firstFacility = $facilities->first();
         if ($firstFacility) {
             $people = $firstFacility->people_facilities()->get();
         } else {
-            $people = [];// まだpeople（利用者が登録されていない時もエラーが出ないようにする）
+            $people = []; // まだpeople（利用者が登録されていない時もエラーが出ないようにする）
         }
-      
-        return view('people',compact('people'));
+
+        return view('people', compact('people'));
     }
 
-       
+
     /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
      */
     public function create()
-{
-    return view('peopleregister');
-}
+    {
+        return view('peopleregister');
+    }
 
 
-   public function store(Request $request)
-{
-    $storeData = $request->validate([
-        'person_name' => 'required|max:255',
-        'date_of_birth' => 'required|max:255',
-        'jukyuusha_number' => 'required|digits:10',
-    ]);
-
-    $directory = 'public/sample';
-    $filename = null;
-    $filepath = null;
-
-    if ($request->hasFile('filename')) {
-        $request->validate([
-            'filename' => 'image|max:2048',
+    public function store(Request $request)
+    {
+        $storeData = $request->validate([
+            'person_name' => 'required|max:255',
+            'date_of_birth' => 'required|max:255',
+            'jukyuusha_number' => 'required|digits:10',
         ]);
-        $filename = uniqid() . '.' . $request->file('filename')->getClientOriginalExtension();
-         $filename = $request->file('filename')->getClientOriginalName();	
-        $request->file('filename')->storeAs($directory, $filename);
-        $filepath = $directory . '/' . $filename;
+
+        $directory = 'public/sample';
+        $filename = null;
+        $filepath = null;
+
+        if ($request->hasFile('filename')) {
+            $request->validate([
+                'filename' => 'image|max:2048',
+            ]);
+            $filename = uniqid() . '.' . $request->file('filename')->getClientOriginalExtension();
+            $filename = $request->file('filename')->getClientOriginalName();
+            $request->file('filename')->storeAs($directory, $filename);
+            $filepath = $directory . '/' . $filename;
+        }
+
+        $newpeople = Person::create([
+            'person_name' => $request->person_name,
+            'date_of_birth' => $request->date_of_birth,
+            'gender' => $request->gender,
+            'jukyuusha_number' => $request->jukyuusha_number,
+            'filename' => $filename,
+            'path' => $filepath,
+
+        ]);
+
+        $user = auth()->user();
+
+        // facility_staffsメソッドからuserの情報をゲットする↓
+        $facilities = $user->facility_staffs()->get();
+        $firstFacility = $facilities->first();
+
+        // 現在ログインしているユーザーが属する施設にpeople（利用者）を紐づける↓
+        // syncWithoutDetaching＝完全重複以外は、重複OK
+        $newpeople->people_facilities()->syncWithoutDetaching($firstFacility->id);
+
+        if ($firstFacility) {
+            $people = $firstFacility->people_facilities()->get();
+        } else {
+            $people = []; // まだpeople（利用者が登録されていない時もエラーが出ないようにする）
+        }
+
+        // 二重送信防止
+        $request->session()->regenerateToken();
+        return view('people', compact('people'));
     }
-
-    $newpeople = Person::create([
-        'person_name' => $request->person_name,
-        'date_of_birth' => $request->date_of_birth,
-        'gender' => $request->gender,
-        'jukyuusha_number' => $request->jukyuusha_number,
-        'filename' => $filename,
-        'path' => $filepath,
-
-    ]);
-    
-    $user = auth()->user();
-    
-    // facility_staffsメソッドからuserの情報をゲットする↓
-    $facilities = $user->facility_staffs()->get();
-    $firstFacility = $facilities->first();
-    
-    // 現在ログインしているユーザーが属する施設にpeople（利用者）を紐づける↓
-    // syncWithoutDetaching＝完全重複以外は、重複OK
-    $newpeople->people_facilities()->syncWithoutDetaching($firstFacility->id);
-    
-    if ($firstFacility) {
-        $people = $firstFacility->people_facilities()->get();
-    } else {
-        $people = [];// まだpeople（利用者が登録されていない時もエラーが出ないようにする）
-    }
-
-    // 二重送信防止
-     $request->session()->regenerateToken();
-    return view('people',compact('people'));
-}
 
 
     /**
@@ -140,7 +141,7 @@ class PersonController extends Controller
      */
     public function show(Person $person)
     {
-    return view('temperature.'.$person->id.'.edit');//
+        return view('temperature.' . $person->id . '.edit'); //
     }
 
 
@@ -150,13 +151,13 @@ class PersonController extends Controller
      * @param  \App\Models\Person  $person
      * @return \Illuminate\Http\Response
      */
-     
+
     // 更新画面の表示↓
     public function edit($id)
-{
-    $person = Person::find($id);
-    return view('peopleedit', compact('person'));
-}
+    {
+        $person = Person::find($id);
+        return view('peopleedit', compact('person'));
+    }
 
     /**
      * Update the specified resource in storage.
@@ -165,51 +166,51 @@ class PersonController extends Controller
      * @param  \App\Models\Person  $person
      * @return \Illuminate\Http\Response
      */
-     
+
     // 体温登録のルート↓
     public function showtemperature(Person $person)
-{
-    $people = Person::all();
+    {
+        $people = Person::all();
         // ('people')に$peopleが代入される
-    return view('temperaturelist',compact('people'));
-} 
+        return view('temperaturelist', compact('people'));
+    }
 
-// 食事
-// 登録のルート↓
+    // 食事
+    // 登録のルート↓
     public function showfood(Person $person)
-{
-    $people = Person::all();
+    {
+        $people = Person::all();
         // ('people')に$peopleが代入される
-    return view('foodlist',compact('people'));
-} 
+        return view('foodlist', compact('people'));
+    }
     //  フォームから送られてきたデータ↓
     public function update(Request $request, Person $person)
     {
-       $storeData = $request->validate([
+        $storeData = $request->validate([
             //  requireは必須項目　nullableは書かなくてもいい
             // 'person_name' => 'required|max:255',
             // 'date_of_birth' => 'required|max:255',
             // 'age' => 'required|max:255',
         ]);
-        
-        $person ->update($updateData);
+
+        $person->update($updateData);
         // トップページに返す↓
         return redirect('/people');
     }
 
 
-    
-public function uploadForm()
+
+    public function uploadForm()
     {
         // return view('people');変更↓
-       return view('peopleregister');
+        return view('peopleregister');
     }
-    
-    
 
 
 
- public function __invoke()
+
+
+    public function __invoke()
     {
         return view('person');
     }
@@ -222,7 +223,7 @@ public function uploadForm()
      */
     public function destroy(Person $person)
     {
-        
+
         $person->delete();
         return redirect('/people');
     }
